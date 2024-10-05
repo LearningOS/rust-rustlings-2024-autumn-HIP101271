96971fa2b57f55a22931ad28d05adfa8b91c5607
HIP101271 <qqqqwer1213@163.com>
2024-10-05 17:09:03 +0800

    solve

--- exercises/strings/strings4.rs
+++ exercises/strings/strings4.rs
@@ -7,9 +7,8 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
 
-fn string_slice(arg: &str) {
+fn sring_slicte(arg: &str) {
     println!("{}", arg);
 }
 fn string(arg: String) {
@@ -17,14 +16,14 @@ fn string(arg: String) {
 }
 
 fn main() {
-    ???("blue");
-    ???("red".to_string());
-    ???(String::from("hi"));
-    ???("rust is fun!".to_owned());
-    ???("nice weather".into());
-    ???(format!("Interpolation {}", "Station"));
-    ???(&String::from("abc")[0..1]);
-    ???("  hello there ".trim());
-    ???("Happy Monday!".to_string().replace("Mon", "Tues"));
-    ???("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    sring_slicte("blue");
+    string("red".to_string());
+    string(String::from("hi"));
+    string("rust is fun!".to_owned());
+    sring_slicte("nice weather".into());
+    string(format!("Interpolation {}", "Station"));
+    sring_slicte(&String::from("abc")[0..1]);
+    sring_slicte("  hello there ".trim());
+    string("Happy Monday!".to_string().replace("Mon", "Tues"));
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase());
 }
